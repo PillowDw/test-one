50cde878f3352ce44d3f76b6bce74fbccbaedf0d
PillowDW <233wozuisaodeliuhai@gmail.com>
2026-08-07 13:01:54 +0800

    feat: update dependencies and add new features
    - Updated Astro and added new dependencies for Preact and RSS support.
    - Enhanced Header component with ThemeIcon for theme toggling.
    - Improved MarkdownPostLayout to display additional post metadata.
    - Modified about page to reflect completion status.
    - Refactored blog page to use getCollection for fetching posts.
    - Added Greeting component for personalized messages on the home page.
    - Deleted old blog posts and replaced with new structured content.
    - Implemented theme toggle functionality with corresponding styles.
    - Set up content collections for blog posts using Astro's content API.
    - Created dynamic routing for blog posts and RSS feed generation.

--- test-one/astro.config.mjs
+++ test-one/astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+
+import preact from "@astrojs/preact";
+
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://diyigetest.netlify.app",
+  integrations: [preact()]
+});
